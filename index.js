861f12fb81cb92be7713344fd4b2d751b6c19b43
mcsrodi <m.c.s.rodi2017@gmail.com>
2023-03-31 20:07:14 +0100

    post and get

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 require("dotenv").config();
 const express = require('express');
 const connectToDb = require("./database/db")
-
+const path = require("path");
+const Music = require("./model/Music");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
 connectToDb();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  const playlist = await Music.find();
+  res.render("index", {playlist});
 });
+
+app.get('/admin', (req, res) => {
+    res.render("admin");
+});
+
+app.post('/create', async (req, res) => {
+    const music = req.body;
+    await Music.create(music);
+    res.redirect("/")
+  });
 
 app.listen(port, () =>
     console.log(`Servidor rodando em http://localhost:${port}`)
